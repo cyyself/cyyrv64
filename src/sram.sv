a3e01631a1dcb7249ae6c68488b4824b680591d1
Yangyu Chen <cyy@cyyself.name>
2022-02-01 19:47:04 +0800

    change sram for better timing

--- src/sram.sv
+++ src/sram.sv
@@ -4,19 +4,18 @@ module sram #(
     parameter DEPTH     = 32,
     parameter INIT_FILE = ""
 ) (
-    input  [LEN_ADDR-1:0]           addra,
-    input                           clka,
-    input  [LEN_DATA-1:0]           dina,
-    output logic [LEN_DATA-1:0]     douta,
-    input                           ena,
-    input  [LEN_DATA/8-1:0]         wea
+    input  [LEN_ADDR-1:0]   addra,
+    input                   clka,
+    input  [LEN_DATA-1:0]   dina,
+    output [LEN_DATA-1:0]   douta,
+    input                   ena,
+    input  [LEN_DATA/8-1:0] wea
 );
 
 (* ram_style="block" *) reg [LEN_DATA-1:0] ram [DEPTH-1:0];
 
 wire [$clog2(DEPTH)-1:0] line_addr = addra[$clog2(LEN_DATA/8) +: $clog2(DEPTH)];
 wire [LEN_DATA-1:0] write_mask;
-wire [LEN_DATA-1:0] new_data = (ram[line_addr] & (~write_mask)) | (dina & write_mask);
 
 genvar i;
 generate
@@ -30,15 +29,24 @@ initial begin
     if (INIT_FILE != "") $readmemh(INIT_FILE, ram);
 end
 
-
+logic [$clog2(DEPTH)-1:0]   line_addr_locked;
+logic [LEN_DATA-1:0]        write_mask_locked;
+logic [LEN_DATA-1:0]        dina_locked;
 
 always_ff @(posedge clka) begin // read
-    if (ena) douta <= new_data;
-    else douta <= 0;
+    if (ena) begin
+        line_addr_locked    <= line_addr;
+        write_mask_locked   <= write_mask;
+        dina_locked         <= dina;
+    end
 end
 
-always_ff @(posedge clka) begin // write
-    if (ena) ram[line_addr] <= new_data;
+wire [LEN_DATA-1:0] new_data = (ram[line_addr_locked] & (~write_mask_locked)) | (dina_locked & write_mask_locked);
+
+assign douta = new_data;
+
+always_ff @(posedge clka) begin // do write at next posedge clk
+    ram[line_addr_locked] <= new_data;
 end
 
 endmodule
