73de306615c8f1b7e5c14c5a351b5a5baaa3a7dd
Yangyu Chen <cyy@cyyself.name>
2022-02-04 17:08:19 +0800

    optimize uart, back to 100MHz on 7a100t-1

--- src/sram_uart_lite.sv
+++ src/sram_uart_lite.sv
@@ -4,7 +4,7 @@ module sram_uart_lite #(
     input        [63:0] addra,
     input               clka,
     input        [63:0] dina,
-    output       [63:0] douta,
+    output logic [63:0] douta,
     input               ena,
     input         [7:0] wea,
     // to phy
@@ -14,17 +14,19 @@ module sram_uart_lite #(
     input         [7:0] rx_data,
     input               rx_ready
 );
-// only for output
-wire [63:0]  addra_prefix = {addra[63:3],3'd0};
-
-assign douta = {16'd0,{1'b0,tx_ready,tx_ready,5'd0},40'd0};
-// 5: UART_LSR_TEMT | UART_LSR_THRE
 
 always_ff @(posedge clka) begin
-    if (addra_prefix == BASE_ADDR && ena && wea[0]) begin
-        tx_data     <= dina[7:0];
-        tx_valid    <= 1;
-        $write("%c",dina[7:0]);
+    if (ena) begin
+        douta <= {16'd0,{1'b0,tx_ready,tx_ready,5'd0},40'd0}; // 5: UART_LSR_TEMT | UART_LSR_THRE
+        if (wea[0]) begin
+            tx_data     <= dina[7:0];
+            tx_valid    <= 1;
+            $write("%c",dina[7:0]);
+        end
+        else begin
+            tx_data     <= 0;
+            tx_valid    <= 0;
+        end
     end
     else begin
         tx_data     <= 0;
