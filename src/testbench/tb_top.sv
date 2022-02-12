feec4c91c5c51d6c784a345f4dfd5f4014b0d565
Yangyu Chen <cyy@cyyself.name>
2022-02-13 02:47:36 +0800

    add verilog output to testbench

--- src/testbench/tb_top.sv
+++ src/testbench/tb_top.sv
@@ -7,15 +7,21 @@ initial #100 rst = 0;
 
 always #5 clk = ~clk;
 
+wire [7:0] uart_tx_data;
+wire       uart_tx_valid;
+wire       uart_rx_ready;
+
 sim_soc_top soc_top(
     .clk            (clk),
     .rst            (rst),
-    .uart_tx_data   (),
-    .uart_tx_valid  (),
+    .uart_tx_data   (uart_tx_data),
+    .uart_tx_valid  (uart_tx_valid),
     .uart_tx_ready  (1'b1),
     .uart_rx_data   (0),
     .uart_rx_valid  (0),
-    .uart_rx_ready  ()
+    .uart_rx_ready  (uart_rx_ready)
 );
+
+always_ff @(posedge clk) if (uart_tx_valid) $write("%c",uart_tx_data);
 
 endmodule
