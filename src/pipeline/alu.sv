ea6e685aa2d615438f66e3b6b8020d2e754cf80b
Yangyu Chen <cyy@cyyself.name>
2022-04-03 21:17:34 +0800

    alu fix

--- src/pipeline/alu.sv
+++ src/pipeline/alu.sv
@@ -5,26 +5,29 @@ module alu(
     input           [63:0]  in_b,
     input                   word32,
     input           [3:0]   aluctrl,
-    output logic    [63:0]  result
+    output          [63:0]  result
 );
 
 wire [63:0] a = word32 ? {{32{in_a[31]}},in_a[31:0]} : in_a;
 wire [63:0] b = word32 ? {{32{in_b[31]}},in_b[31:0]} : in_b;
 
+logic [63:0] out;
+assign result = word32 ? {{32{out[31]}},out[31:0]} : out;
+
 always_comb begin
     case (aluctrl)
-        `ALU_ADD:   result = a + b;
-        `ALU_SUB:   result = a - b;
-        `ALU_SLL:   result = a << b;
-        `ALU_SLT:   result = {63'd0,$signed(a) < $signed(b)};
-        `ALU_SLTU:  result = {63'd0,a < b};
-        `ALU_XOR:   result = a ^ b;
-        `ALU_SRL:   result = a >> b;
-        `ALU_SRA:   result = $signed(a) >> b;
-        `ALU_OR:    result = a | b;
-        `ALU_AND:   result = a & b;
-        `ALU_IMM:   result = b;
-        default:    result = 0;
+        `ALU_ADD:   out = a + b;
+        `ALU_SUB:   out = a - b;
+        `ALU_SLL:   out = a << b;
+        `ALU_SLT:   out = {63'd0,$signed(a) < $signed(b)};
+        `ALU_SLTU:  out = {63'd0,a < b};
+        `ALU_XOR:   out = a ^ b;
+        `ALU_SRL:   out = a >> b;
+        `ALU_SRA:   out = $signed(a) >> b;
+        `ALU_OR:    out = a | b;
+        `ALU_AND:   out = a & b;
+        `ALU_IMM:   out = b;
+        default:    out = 0;
     endcase
 end
 
